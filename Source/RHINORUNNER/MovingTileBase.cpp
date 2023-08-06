a95384304e77d216709bc9fa8a97f45c9225f1ec
Dee Gordon McCrary III <97470577+gordomcc@users.noreply.github.com>
2023-08-06 09:25:29 -0500

    working moving tiles

--- Source/RHINORUNNER/MovingTileBase.cpp
+++ Source/RHINORUNNER/MovingTileBase.cpp
@@ -9,12 +9,42 @@ AMovingTileBase::AMovingTileBase()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	
 }
 
 // Called when the game starts or when spawned
 void AMovingTileBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	TileMeshes.SetNum(GridSizeX * GridSizeY);
+	
+
+	Tiles.SetNum(GridSizeX * GridSizeY);
+
+	for (int j = 0; j < Tiles.Num(); j++)
+	{
+		
+		for (int i = 0; i < GridSizeX; i++)
+		{
+			for (int ii = 0; ii < GridSizeY; ii++)
+			{
+			
+				Tiles[j].Column = ii; 
+				Tiles[j].Row = i;
+				Tiles[j].Mesh = TileMeshes[j];
+			}
+		}
+	}
+
+	for (int i = 0; i < Tiles.Num(); i++)
+	{
+		DrawTile(12, 12, Tiles[1]);
+		UE_LOG(LogTemp, Warning, TEXT("Grid x : % i, and Grid y : % i"), Tiles[i].Row, Tiles[i].Column);
+	}
+	
+	
 	
 }
 
@@ -22,6 +52,19 @@ void AMovingTileBase::BeginPlay()
 void AMovingTileBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+}
+
+void AMovingTileBase::DrawTile(float xSize, float ySize, FTiles& t)
+{
+	//draw tile to the screen; 
+	float TileRow = t.Row;
+	float TileColumn = t.Column;
+	
+	FVector location = FVector(xSize * TileRow, ySize * TileColumn, t.Mesh->GetRelativeLocation().Z * 0.5f);
+	
+	t.Mesh->SetWorldLocation(location);
+	
 
 }
 
--- Source/RHINORUNNER/MovingTileBase.h
+++ Source/RHINORUNNER/MovingTileBase.h
@@ -6,6 +6,27 @@
 #include "GameFramework/Actor.h"
 #include "MovingTileBase.generated.h"
 
+UENUM()
+enum class TileType
+{
+	NONE,
+	Building,
+	Road,
+
+};
+
+USTRUCT()
+struct FTiles
+{
+	GENERATED_BODY()
+
+	int32 Row;
+	int32 Column;
+	float size;
+	FVector Location;
+	UStaticMeshComponent* Mesh;
+};
+
 UCLASS()
 class RHINORUNNER_API AMovingTileBase : public AActor
 {
@@ -15,12 +36,30 @@ public:
 	// Sets default values for this actor's properties
 	AMovingTileBase();
 
+	
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* TileMesh;
+	TArray<UStaticMeshComponent*> TileMeshes;
+
+	TArray<FTiles> Tiles; 
+
+	UPROPERTY(EditAnywhere)
+		int32 GridSizeX = 5;
+	UPROPERTY(EditAnywhere)
+		int32 GridSizeY = 5;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	
+
+	UFUNCTION()
+	void DrawTile(float xSize, float ySize, FTiles& t);
 
 };
